888a3e04f142b007c092fb4bf586d5d794b1ca2d
Cynede <cynede@gentoo.org>
2019-03-14 15:26:00 +0400

    minor improvements

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -17,8 +17,8 @@ command!(partners(_ctx, msg) {
   for line in lines {
     let split : Vec<&str> = line.split('|').collect();
     if split.len() > 2 {
-      set! { partner_description  = split[0]
-           , partner_owner        = split[1]
+      let mut partner_description  = split[0];
+      set! { partner_owner        = split[1]
            , partner_invite       = split[2] };
       let mut easy = Easy::new();
       let app_invite = String::from("https://discordapp.com/invite/") + partner_invite;
@@ -48,11 +48,39 @@ command!(partners(_ctx, msg) {
 
       let invite_regex = Regex::new(r#"og:image" content="(.*)\?size="#).unwrap();
       let caps = invite_regex.captures(invite_content).unwrap();
-
       let thumbnail = if caps.len() > 0 { caps.get(1).map_or("", |m| m.as_str()) }
         else { "https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png" };
 
       let invite_link = String::from("https://discord.gg/") + partner_invite;
+
+      set! { desc_regex = Regex::new(r#"og:description" content="(.*) \|"#).unwrap()
+           , desc_caps = desc_regex.captures(invite_content) };
+      let description =
+        match desc_caps {
+          Some(x) => if x.len() > 0 { x.get(1).map_or("", |m| m.as_str()) } else { "" },
+          None    => {
+            let temp = partner_description;
+            partner_description = "-";
+            temp
+          }
+       };
+
+      set! { mc_regex = Regex::new(r#"\| (.*) members"#).unwrap()
+           , mc_caps = mc_regex.captures(invite_content) };
+      let members =
+        match mc_caps {
+          Some(x) => if x.len() > 0 { x.get(1).map_or("", |m| m.as_str()) } else { "" },
+          None    => {
+            set! { mc_regex2 = Regex::new(r#"with (.*) other members"#).unwrap()
+                 , mc_caps2 = mc_regex2.captures(invite_content) };
+            match mc_caps2 {
+              Some(xx) => if xx.len() > 0 { xx.get(1).map_or("", |m| m.as_str()) } else { "" },
+              None    => {
+                "-"
+              }
+           }
+          }
+        };
 
       set! { red    = rand::thread_rng().gen_range(0, 255)
            , green  = rand::thread_rng().gen_range(0, 255)
@@ -62,10 +90,14 @@ command!(partners(_ctx, msg) {
         .embed(|e| e
           .title(title_fixed)
           .thumbnail(thumbnail)
-          .description(partner_description)
+          .description(description)
           .fields(vec![
             ("Owner", partner_owner, true),
             ("Invite", invite_link.as_str(), true)
+            ])
+          .fields(vec![
+            ("Members", members, true),
+            ("Note", partner_description, true)
             ])
           .colour((red, green, blue)))) {
         error!("Error posting partner: {:?}", why);
