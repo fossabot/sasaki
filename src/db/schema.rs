d96a2a7ac30e726e57e117f31d3c9546db45c7f9
Cynede <cynede@gentoo.org>
2019-04-02 16:33:18 +0400

    db create instruction change

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,5 +1,5 @@
 // cockroach sql --insecure --database=sasaki_users --user=cnd -e 'CREATE TABLE accounts (id bigint PRIMARY KEY, guild bigint, mute boolean default false)'
-// cockroach sql --insecure --database=sasaki_users --user=cnd -e 'CREATE TABLE user_roles (user_id bigint, role_id bigint, PRIMARY KEY (user_id, role_id))'
+// cockroach sql --insecure --database=sasaki_users --user=cnd -e 'CREATE TABLE user_roles (id bigint, role_id bigint, PRIMARY KEY (id, role_id))'
 // cockroach sql --insecure --database=sasaki_users --user=cnd -e 'CREATE TABLE todo (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), user_id bigint, text text, FOREIGN KEY (user_id) REFERENCES accounts (id))'
 
 table! {
